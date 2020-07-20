8c3bc67f2c296f8a7616f788b15bff73785b3fde
İsa Kördiş <30376108+isakordis@users.noreply.github.com>
2020-07-20 18:16:01 +0300

    Update mailController.js

--- controller/mailController.js
+++ controller/mailController.js
@@ -4,13 +4,13 @@ var transport = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
     port: 2525,
     auth: {
-      user: "004698d8442a80",
-      pass: "68ce180ad2140b"
+      user: "----",
+      pass: "-----"
     }
   });
 
 const message = {
-    from: 'ikordis@netas.com.tr', // Sender address
+    from: 'isa.kordis02@gmail.com', // Sender address
     to: 'isa-75f32e@inbox.mailtrap.io',         // List of recipients
     subject: 'Deneme', // Subject line
     text: 'Deneme Mail ' // Plain text body
